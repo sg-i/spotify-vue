dfe0b35a4965a854e3948ccf53f6ad5eb169ff50
4017 Сгадов И.В <sgadov2vanya@yandex.ru>
2024-08-28 15:48:20 +0300

    fixed random images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HomeCard from '@/components/HomeCard.vue'
-const randomImageLink = `https://picsum.photos/300/300?random=`
+const randomImageLink = `https://loremflickr.com/300/300/abstract?lock=`
+const N = 1000
 </script>
 
 <template>
@@ -13,27 +14,27 @@ const randomImageLink = `https://picsum.photos/300/300?random=`
 
     <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-6">
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Serene Sunset"
         subTitle="Nature's Beauty"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Tech Innovations"
         subTitle="Shaping Tomorrow"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Urban Explorations"
         subTitle="City Vibes"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Whispers of Wisdom"
         subTitle="Timeless Tales"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Mystic Moonlight"
         subTitle="Night's Embrace"
       />
@@ -48,27 +49,27 @@ const randomImageLink = `https://picsum.photos/300/300?random=`
 
     <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-6">
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Digital Dreams"
         subTitle="Virtual Realities"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Whimsical Wonderland"
         subTitle="Fantasy Adventure"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Harmony in Chaos"
         subTitle="Balancing Act"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Ethereal Echoes"
         subTitle="Memories Unfolding"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Journey Beyond Stars"
         subTitle="Cosmic Odyssey"
       />
@@ -83,27 +84,27 @@ const randomImageLink = `https://picsum.photos/300/300?random=`
 
     <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-6">
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Digital Dreams"
         subTitle="Virtual Realities"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Whimsical Wonderland"
         subTitle="Fantasy Adventure"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Harmony in Chaos"
         subTitle="Balancing Act"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Ethereal Echoes"
         subTitle="Memories Unfolding"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Journey Beyond Stars"
         subTitle="Cosmic Odyssey"
       />
@@ -118,27 +119,27 @@ const randomImageLink = `https://picsum.photos/300/300?random=`
 
     <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-6">
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Digital Dreams"
         subTitle="Virtual Realities"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Whimsical Wonderland"
         subTitle="Fantasy Adventure"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Harmony in Chaos"
         subTitle="Balancing Act"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Ethereal Echoes"
         subTitle="Memories Unfolding"
       />
       <HomeCard
-        :image="`${randomImageLink}+${Math.random()}`"
+        :image="`${randomImageLink}${Math.random() * N}`"
         title="Journey Beyond Stars"
         subTitle="Cosmic Odyssey"
       />
